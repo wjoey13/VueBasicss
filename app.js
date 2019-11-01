08c011366c2fca1656a53be52b51a20181fc578a
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2019-11-01 17:36:09 -0600

    shaking off the Vue rust :neckbeard:

---
+++ app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Becoming a Vue ninja',
+        items: ['mushroom', 'blue shell', 'red shell', 'banana', 'octopus'],
+        ninjas: [
+            { name: 'Steve', age: 20, belt: 'Rainbow' },
+            { name: 'Marsha', age: 23, belt: 'Dark Pink' },
+            { name: 'Jimbob', age: 45, belt: 'Black' }
+        ]
+    },
+    methods: {
+
+    }
+})
